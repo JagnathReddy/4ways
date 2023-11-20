73b0f9030b0c68fc2d95bb580250dfbee2763135
jagnath reddy <98260819+JagnathReddy@users.noreply.github.com>
2023-11-20 10:25:23 +0530

    updated

--- db/firebase.js
+++ db/firebase.js
@@ -8,14 +8,14 @@ const { getDatabase } = require("firebase/database");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6JmrA1EuGebh_72fqag3GLLUCqf-yY7w",
-  authDomain: "fourways-4ways.firebaseapp.com",
-  databaseURL: "https://fourways-4ways-default-rtdb.firebaseio.com",
-  projectId: "fourways-4ways",
-  storageBucket: "fourways-4ways.appspot.com",
-  messagingSenderId: "247288921432",
-  appId: "1:247288921432:web:09db5ba6250f7dfddff92d",
-  measurementId: "G-SJQ0YK6FK2"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
